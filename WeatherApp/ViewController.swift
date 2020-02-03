cbd152e7faadc34ffb1e4e092a32532d36f0d5a1
Christian Jäderberg <christianjaderberg@gmail.com>
2020-02-03 14:40:24 +0100

    Initial Commit

---
+++ WeatherApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherApp
+//
+//  Created by Christian Jäderberg on 2020-02-03.
+//  Copyright © 2020 Christian Jäderberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
